2352cb18a11a6118e6d816a76a149feccc64af28
Антон Михалев <anton150801@gmail.com>
2022-02-27 19:49:37 +0300

    TSK-003 Добавлены DTO классы для контроллера

---
+++ src/main/java/ru/b19513/pet_schedule/controller/entity/enums/Gender.java
@@ -0,0 +1,8 @@
+package ru.b19513.pet_schedule.controller.entity.enums;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Пол")
+public enum Gender {
+    MALE, FEMALE
+}
