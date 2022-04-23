84e84b0750cf94ae9d65917ada20180ffaaef999
Антон Михалев <anton150801@gmail.com>
2022-04-23 12:07:31 +0300

    [TSK-010] Добавил @mapping в updateEntity

--- src/main/java/ru/b19513/pet_schedule/service/mapper/NotificationMapper.java
+++ src/main/java/ru/b19513/pet_schedule/service/mapper/NotificationMapper.java
@@ -3,27 +3,20 @@ package ru.b19513.pet_schedule.service.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import ru.b19513.pet_schedule.controller.entity.NotificationDTO;
 import ru.b19513.pet_schedule.controller.entity.NotificationScheduleDTO;
 import ru.b19513.pet_schedule.controller.entity.NotificationTimeoutDTO;
-import ru.b19513.pet_schedule.repository.entity.Notification;
 import ru.b19513.pet_schedule.repository.entity.NotificationSchedule;
 import ru.b19513.pet_schedule.repository.entity.NotificationTimeout;
-
-
-import java.util.Collection;
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface NotificationMapper {
     @Mapping(target = "times", expression = "java(entity.getTimes().forEach(t -> times.add(t.getNotifTime())))")
     NotificationScheduleDTO entityToDTO(NotificationSchedule entity);
 
+    @Mapping(target = "times", expression = "java(entity.getTimes().forEach(t -> times.add(t.getNotifTime())))")
     void updateEntity(@MappingTarget NotificationSchedule entity, NotificationScheduleDTO src);
 
     NotificationTimeoutDTO entityToDTO(NotificationTimeout entity);
-
-    List<NotificationDTO> entityToDTO(Collection<Notification> entities);
 
     void updateEntity(@MappingTarget NotificationTimeout entity, NotificationTimeoutDTO src);
 }
