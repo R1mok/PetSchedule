4f2b31b7b650848139f017b42e1c8d73a7b99f1a
R1mok <anton150801@gmail.com>
2022-07-05 17:28:40 +0500

    [TSK-015] Добавил petUpdate тест

--- src/test/java/ru/b19513/pet_schedule/service/PetServiceTest.java
+++ src/test/java/ru/b19513/pet_schedule/service/PetServiceTest.java
@@ -2,21 +2,33 @@ package ru.b19513.pet_schedule.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import ru.b19513.pet_schedule.controller.entity.PetDTO;
 import ru.b19513.pet_schedule.controller.entity.enums.Gender;
 import ru.b19513.pet_schedule.controller.entity.enums.PetType;
 import ru.b19513.pet_schedule.repository.GroupRepository;
 import ru.b19513.pet_schedule.repository.PetRepository;
 import ru.b19513.pet_schedule.repository.entity.Group;
+import ru.b19513.pet_schedule.repository.entity.Pet;
+import ru.b19513.pet_schedule.service.*;
+import ru.b19513.pet_schedule.service.mapper.PetMapper;;
 
 import javax.transaction.Transactional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class PetServiceTest {
 
     @Autowired
     PetRepository petRepository;
+
+    @Autowired
+    PetMapper petMapper;
 
     @Autowired
     GroupRepository groupRepository;
@@ -39,8 +51,34 @@ class PetServiceTest {
         groupRepository.delete(group1);
         petRepository.delete(pet);
     }
+
     @Test
+    @Transactional
     void updatePet() {
+        Group group = Group.builder()
+                .name("g1")
+                .build();
+        groupRepository.save(group);
+        var groupFromRepos = groupRepository.findAll().stream().findAny().get();
+        Pet pet = Pet.builder()
+                .name("Barsik")
+                .gender(ru.b19513.pet_schedule.repository.entity.enums.Gender.MALE)
+                .type(ru.b19513.pet_schedule.repository.entity.enums.PetType.CAT)
+                .group(groupFromRepos)
+                .build();
+        petRepository.save(pet);
+        var petId = petRepository.findAll().stream().findAny().get().getId();
+        PetDTO petUpd = PetDTO.builder()
+                .id(petId)
+                .name("Snezhinka")
+                .gender(Gender.FEMALE)
+                .type(PetType.CAT)
+                .build();
+        petService.updatePet(petUpd);
+        var newPet = petRepository.findAll().stream().findAny().get();
+        Assertions.assertEquals(petMapper.entityToDTO(newPet), petUpd);
+        petRepository.delete(newPet);
+        groupRepository.delete(group);
     }
 
     @Test
