9213ccfcadc4d59cda01dd27cc6c9cb819aa6f88
R1mok <anton150801@gmail.com>
2022-07-06 18:41:08 +0500

    [TSK-016] Добавил OpenAPI Swagger v3, описание и методы

--- src/main/java/ru/b19513/pet_schedule/config/SwaggerConfig.java
+++ src/main/java/ru/b19513/pet_schedule/config/SwaggerConfig.java
@@ -1,5 +1,11 @@
 package ru.b19513.pet_schedule.config;
 
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +33,17 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .build().apiInfo(getApiInfo());
     }
 
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI().info(new Info().
+                title("PetSchedule")
+                .description("RestFULL сервис для управления питанием питомцев")
+                .version("1.0.0")
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                        .name("Anton 'R1mok' Mikhalev")
+                        .email("mikhalevaa15@gmail.com")
+                        .url("https://github.com/R1mok")));
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
@@ -39,14 +56,13 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-
     private ApiInfo getApiInfo() {
         return new ApiInfo(
-                "Расписание кормления",
-                "RESTful сервис для управления напоминаниями о кормлении животных",
+                "PetSchedule",
+                "Сервис для управления напоминаниями о кормлении домашних питомцев",
                 "V1",
                 "urn:tos",
-                new Contact("Jawa", "", "jawa@prog.com"),
+                new Contact("", "", ""),
                 "CC BY-SA 3.0",
                 "https://creativecommons.org/licenses/by-sa/3.0/",
                 Collections.emptyList()
