311607cc569a62639579dadc51eb83162af8bac6
R1mok <anton150801@gmail.com>
2022-07-09 20:29:30 +0500

    [TSK-015] Изменил InvitationMapper

--- src/main/java/ru/b19513/pet_schedule/service/mapper/InvitationMapper.java
+++ src/main/java/ru/b19513/pet_schedule/service/mapper/InvitationMapper.java
@@ -1,15 +1,24 @@
 package ru.b19513.pet_schedule.service.mapper;
 
 import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import ru.b19513.pet_schedule.controller.entity.GroupDTO;
 import ru.b19513.pet_schedule.controller.entity.InvitationDTO;
 import ru.b19513.pet_schedule.repository.entity.Invitation;
-
 import java.util.Collection;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface InvitationMapper {
-    InvitationDTO entityToDTO(Invitation entity);
+
+    GroupMapper groupMapper = new GroupMapperImpl();
+    default InvitationDTO entityToDTO(Invitation entity) {
+        return InvitationDTO.builder()
+                .userId(entity.getUser().getId())
+                .group(groupMapper.entityToDTO(entity.getGroup()))
+                .build();
+    }
 
     List<InvitationDTO> entityToDTO(Collection<Invitation> entities);
 }
